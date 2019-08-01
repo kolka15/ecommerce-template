d5c0c2e45ed7815db2d585ffd9f7a988e133bd62
d.kozhushko <d.kozhushko@informika.ru>
2019-08-01 15:28:24 +0300

    index page: menu items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from "./homepage"
-import './homepage.styles.scss'
+import HomePage from "./pages/homepage"
 
 function App() {
   return (
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Direcory from "../components/directory/direcory"
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Direcory/>
+    </div>
+)
+
+export default HomePage
