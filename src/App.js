812104934c8a9efdf0e3758f4f24c49512fc517e
d.kozhushko <d.kozhushko@informika.ru>
2019-08-05 12:30:00 +0300

    form input component, button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage"
 import ShopPage from "./pages/shop/shop"
 import Header from "./components/header/header"
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up"
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
+                <Route path='/sign-in' component={SignInAndSignUpPage}/>
             </Switch>
         </div>
     )
