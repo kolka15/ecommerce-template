c33ee5fc2b8df4cf23df5769924c693e08656fcf
d.kozhushko <d.kozhushko@informika.ru>
2019-08-06 16:35:12 +0300

    sign in

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './sign-in.scss';
 
-import {signInWithGoogle} from "../../firebase/firebase"
+import {signInWithGoogle, auth} from "../../firebase/firebase"
 import FormInput from '../form-input/form-input';
 import CustomButton from "../custom-button/custom-button"
 
@@ -16,10 +16,19 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' });
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' });
+
+        } catch (e) {
+            console.error(e)
+        }
+
     };
 
     handleChange = event => {
