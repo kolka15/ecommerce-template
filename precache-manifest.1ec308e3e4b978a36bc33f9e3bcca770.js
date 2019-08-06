392cb98a72264db894ed42d0a65f4acd20212bb1
d.kozhushko <d.kozhushko@informika.ru>
2019-08-06 17:37:10 +0300

    Updates

---
+++ precache-manifest.1ec308e3e4b978a36bc33f9e3bcca770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4eb15c1358d07ce973ae348fca6f270",
+    "url": "/ecommerce-template/index.html"
+  },
+  {
+    "revision": "f821de75f1268a2d5dd3",
+    "url": "/ecommerce-template/static/css/main.c97cec0c.chunk.css"
+  },
+  {
+    "revision": "ec8f46aeff578e2a308e",
+    "url": "/ecommerce-template/static/js/2.461514e6.chunk.js"
+  },
+  {
+    "revision": "f821de75f1268a2d5dd3",
+    "url": "/ecommerce-template/static/js/main.923a443f.chunk.js"
+  },
+  {
+    "revision": "6a7dba4d039ccb40bb7d",
+    "url": "/ecommerce-template/static/js/runtime~main.50613f53.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce-template/static/media/crown.ed28accb.svg"
+  }
+]);
