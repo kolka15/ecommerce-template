25e96c0a6f9c05ebca110080a8daa126dc237c03
d.kozhushko <d.kozhushko@informika.ru>
2019-08-22 19:21:41 +0300

    add to cart action

--- src/components/directory/direcory.js
+++ src/components/directory/direcory.js
@@ -12,7 +12,7 @@ class Direcory extends Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'hats'
+                    linkUrl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
