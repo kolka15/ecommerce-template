6e7a3e866d651c50ea94d9a70d67773ea988d22c
d.kozhushko <d.kozhushko@informika.ru>
2019-08-30 10:21:20 +0300

    merchandise data transformed to object

--- src/components/collections-overview/collections-overview.js
+++ src/components/collections-overview/collections-overview.js
@@ -3,20 +3,20 @@ import './collections-overview.scss'
 import {connect} from 'react-redux'
 import {createStructuredSelector} from "reselect"
 import CollectionPreview from "../collection-preview/collection-preview"
-import {selectCollections} from "../../redux/shop/shop.selectors"
+import {selectCollectionsForPreview} from "../../redux/shop/shop.selectors"
 
 const CollectionsOverview = ({collections}) => (
     <div className='collections-overview'>
         {
-            Object.keys(collections).map((key) => (
-                <CollectionPreview key={collections[key].id} {...collections[key]}/>
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}/>
             ))
         }
     </div>
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
