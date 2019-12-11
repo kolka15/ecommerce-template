1700c3a661b8c350467b6009ab5ca7004b0c68d6
d.kozhushko <d.kozhushko@informika.ru>
2019-12-11 15:48:23 +0300

    Updates

---
+++ precache-manifest.8f804033f329442891218f4e8a6fd32c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a14d453f8d3663c32077535fc50d16",
+    "url": "/ecommerce-template/index.html"
+  },
+  {
+    "revision": "098d04df95491ead6775",
+    "url": "/ecommerce-template/static/css/3.f51a31b5.chunk.css"
+  },
+  {
+    "revision": "c7841756f3636502e23c",
+    "url": "/ecommerce-template/static/css/4.f601127e.chunk.css"
+  },
+  {
+    "revision": "d88dc9cf240aef67385d",
+    "url": "/ecommerce-template/static/css/5.daae9388.chunk.css"
+  },
+  {
+    "revision": "71fc24a4e86fe3db4216",
+    "url": "/ecommerce-template/static/css/6.7fd0d055.chunk.css"
+  },
+  {
+    "revision": "17599b47c143acb558ba",
+    "url": "/ecommerce-template/static/css/7.5d850e88.chunk.css"
+  },
+  {
+    "revision": "2ab338acac427af67929",
+    "url": "/ecommerce-template/static/css/8.6afa580b.chunk.css"
+  },
+  {
+    "revision": "17366bb74ba1187e7b96",
+    "url": "/ecommerce-template/static/css/main.b5f877cb.chunk.css"
+  },
+  {
+    "revision": "567ae2c737422e959720",
+    "url": "/ecommerce-template/static/js/2.51f15e72.chunk.js"
+  },
+  {
+    "revision": "098d04df95491ead6775",
+    "url": "/ecommerce-template/static/js/3.1dd0c18d.chunk.js"
+  },
+  {
+    "revision": "c7841756f3636502e23c",
+    "url": "/ecommerce-template/static/js/4.e327bd61.chunk.js"
+  },
+  {
+    "revision": "d88dc9cf240aef67385d",
+    "url": "/ecommerce-template/static/js/5.b81c43cc.chunk.js"
+  },
+  {
+    "revision": "71fc24a4e86fe3db4216",
+    "url": "/ecommerce-template/static/js/6.fdb3979f.chunk.js"
+  },
+  {
+    "revision": "17599b47c143acb558ba",
+    "url": "/ecommerce-template/static/js/7.3051d26d.chunk.js"
+  },
+  {
+    "revision": "2ab338acac427af67929",
+    "url": "/ecommerce-template/static/js/8.70b029c2.chunk.js"
+  },
+  {
+    "revision": "17366bb74ba1187e7b96",
+    "url": "/ecommerce-template/static/js/main.e82b97f3.chunk.js"
+  },
+  {
+    "revision": "9b7375bd2e95debfc2aa",
+    "url": "/ecommerce-template/static/js/runtime~main.b154fdf3.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce-template/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "d66f0ff3e7b495f251d4524fa7dcc9af",
+    "url": "/ecommerce-template/static/media/shopping-bag.d66f0ff3.svg"
+  }
+]);
